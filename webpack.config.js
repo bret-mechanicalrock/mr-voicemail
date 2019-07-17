b747f32abaeab93a9bfac717c229e041a47cd937
Bret Waldow <bret.waldow@mechanicalrock.io>
2019-07-17 16:50:03 +0800

    Initial project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+		// We no not want to minimize our code.
+		minimize: false
+	},
+  entry: slsw.lib.entries,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  externals: [
+    nodeExternals()
+  ],
+  target: 'node',
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+};
